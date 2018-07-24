8692430d243fd005f0ee7b9222b4416b1f649857
agrajak <korbots@gmail.com>
2018-07-24 18:10:21 +0900

    따봉페이지 추가했음

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="login container" style="margin:50px 0px;">
-    <div v-show="!isLogged" class="col-sm-4">
-        <form>
-            <div class="form-group">
-                <label for="id">학번</label>
-                <input class="form-control" id="id" aria-describedby="emailHelp" placeholder="Enter ID" v-model="id">
-                <small id="emailHelp" class="form-text text-muted"></small>
-            </div>
-            <div class="form-group">
-                <label for="password">비밀번호</label>
-                <input type="password" class="form-control" id="password" placeholder="Password" v-model="password">
-                <small id="emailHelp" class="form-text text-muted">초기비밀번호는 학번입니다.</small>
-            </div>
-            <button class="btn btn-primary" @click.prevent="submit">로그인</button>
-        </form>
+    <div class="row">
+        <div class="col-sm-4"></div>
+        <div v-show="!isLogged" class="col-sm-4">
+            <form>
+                <div class="form-group">
+                    <label for="id">학번</label>
+                    <input class="form-control" id="id" aria-describedby="emailHelp" placeholder="Enter ID" v-model="id">
+                    <small id="emailHelp" class="form-text text-muted"></small>
+                </div>
+                <div class="form-group">
+                    <label for="password">비밀번호</label>
+                    <input type="password" class="form-control" id="password" placeholder="Password" v-model="password">
+                    <small id="emailHelp" class="form-text text-muted">초기비밀번호는 학번입니다.</small>
+                </div>
+                <button class="btn btn-primary" @click.prevent="submit">로그인</button>
+            </form>
+        </div>
     </div>
     <div v-show="isLogged">
         이미 로그인되어있어용
     </div>
+
   </div>
 </template>
 
--- src/components/PostUploader.vue
+++ src/components/PostUploader.vue
@@ -2,26 +2,25 @@
   <div>
     <div class="unlogin-box container" v-show="!isLogged">
       <div class="alert alert-warning" role="alert" >로그인이 필요합니다!</div>
-        <router-link tag="a" :to="{name: 'Login'}">클릭 시 로그인페이지로 이동합니다</router-link>
+      <router-link tag="a" :to="{name: 'Login'}">클릭 시 로그인페이지로 이동합니다</router-link>
     </div>
     <div v-show="isLogged">
-        <div class="board-box container">
-          <button type="button" class="btn btn-outline-primary" style="float:right" @click.prevent="submitLog">{{mode=='create'?'등록':'수정'}}</button>
-          <button type="button" class="btn btn-outline-secondary" style="float:right" @click="$router.go(-1)">뒤로가기</button>
-          <form>
-            <div class="form-group">
-              <input type="file" ref="file" id="files" class="form-control-file" @change="fileChanges">
-            </div>
-          </form>
-          <form>
-            <div class="form-group">
-              <input v-model="title" class="form-control" id="exampleFormControlInput1" placeholder="title">
-            </div>
-            <div class="form-group">
-              <textarea v-model="content" class="form-control" id="exampleFormControlTextarea1" placeholder="content" rows="3"></textarea>
-            </div>
-          </form>
-        </div>
+      <div class="board-box container">
+        <button type="button" class="btn btn-outline-primary" style="float:right" @click.prevent="submitLog">{{mode=='create'?'등록':'수정'}}</button>
+        <button type="button" class="btn btn-outline-secondary" style="float:right" @click="$router.go(-1)">뒤로가기</button>
+        <form>
+          <div class="form-group">
+            <input type="file" ref="file" id="files" class="form-control-file" @change="fileChanges">
+          </div>
+        </form>
+        <form>
+          <div class="form-group">
+            <input v-model="title" class="form-control" id="exampleFormControlInput1" placeholder="title">
+          </div>
+          <div class="form-group">
+            <textarea v-model="content" class="form-control" id="exampleFormControlTextarea1" placeholder="content" rows="3"></textarea>
+          </div>
+        </form>
       </div>
     </div>
   </div>
