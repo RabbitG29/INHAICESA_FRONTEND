d2b5589d327020475761c6c5451517880ded74dc
RabbitG29 <recoder1212@naver.com>
2018-07-28 18:18:18 +0900

    asa

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
                     <div class="row">
                         <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
                             <div id="navigation">
-                                <router-link to="/"><img src="./components/home.png" width="70%" hspace=30/></router-link>
+                                  <a href="/"><img src="./components/home.png" width="70%" hspace=30/></a>
                             </div>
                         </div>
                         <div class="col-lg-8 col-md-4 col-sm-12 col-xs-12">
