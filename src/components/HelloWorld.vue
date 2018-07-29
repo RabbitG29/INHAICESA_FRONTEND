3847e2e9a8f24c164ad79ca0d5aab16e4fb1d6d7
RabbitG29 <recoder1212@naver.com>
2018-07-29 16:39:07 +0900

    carousel 추가, img 파일경로 정리

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="hello container">
+  <div class="hello container" align="center">
+    <br>
+    <h1>정통 학생회에 오신 것을 환영합니다</h1>
+    <br>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" >
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="./images/OT1.jpg" alt="First slide" >
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="./images/OT2.jpg" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="./images/Conference.jpg" alt="Third slide">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
     <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Finhaicesa%2F&tabs=timeline&width=500&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="500" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
   </div>
 </template>
@@ -30,5 +54,13 @@ li {
 }
 a {
   color: #42b983;
+}
+.hello container {
+    vertical-align: middle;
+}
+#carouselExampleControls {
+
+  width: 500px;
+  height: auto;
 }
 </style>
